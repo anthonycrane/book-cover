52cb90e88183ed8b52d3c90e3608e74063899272
boyercb <cboyer@poverty-action.org>
2016-01-03 17:15:37 -0500

    Convert image to binary array and append to mongodb data base.

--- python/covers.py
+++ python/covers.py
@@ -1,18 +1,19 @@
 import requests
+import pickle
 from PIL import Image
 from numpy import array
 from pymongo import MongoClient
 from progressbar import ProgressBar, Percentage
+from bson.binary import Binary
 
 # function defintions
-def getImage(url, fout, pbar):
-	"""retrieves book cover image file from url"""
+def get_array(url, fout, pbar):
+	"""retrieves book cover image file from url and converts it to numpy array"""
 	r = requests.get(url, stream = True)
-	with open(fout, 'wb') as fd:
-		for chunk in r:
-			fd.write(chunk)
-			pbar.update()
-
+	img = Image.open(r.raw).thumbnail((255, 255), Image.ANTIALIAS)
+	ar = array(img)
+	pbar.update()
+	return ar
 
 # open connection to mongod
 client = MongoClient()
@@ -33,7 +34,9 @@ pbar = ProgressBar( max_value = 100, poll = 0.0001)
 # loop through k meta entries and download image
 for item in pbar(cursor[1:100]):
 	if 'imUrl' in item:
-		getImage(item['imUrl'], '../img/' + item['asin'] + '.jpg', pbar)
-
+		img_array = get_array(item['imUrl'], '../img/' + item['asin'] + '.jpg', pbar)
+		item['img'] = Binary(pickle.dumps(img_array, protocol = 2), subtype = 128)
+		meta.save(item)
 	else:
 		print(item['asin'] + '.jpg no image found')
+
